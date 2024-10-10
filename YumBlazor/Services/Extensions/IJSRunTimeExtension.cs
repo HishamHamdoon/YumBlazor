04e5806ddb1d661fd4ad3af2738e06b984e50d56
=hisham <=hisham@gmail.com.sa>
2024-10-10 22:31:05 +0300

    add category and product modules

---
+++ YumBlazor/Services/Extensions/IJSRunTimeExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRunTimeExtension
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToaster", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToaster", "error", message);
+        }
+        //public static async Task ToastrSuccess(this IJSRuntime js,string message)
+        //{
+
+        //    await js.InvokeVoidAsync("showToastr", "success", message);
+        //}
+        //public static async Task ToastrError(this IJSRuntime js, string message)
+        //{
+        //    await js.InvokeVoidAsync("showToastr", "error",message);
+        //}
+    }
+}
---
+++ YumBlazor/wwwroot/Js/ToggleModal.js
@@ -0,0 +1,7 @@
+﻿function ShowConfimationModal()
+{
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('deleteModal').show();
+}
+function HideConfimationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('deleteModal').hide();
+}
